6a3ec13f5427f59852b2ac3fe0ba563c0d950528
mikidevs <mikis168@protonmail.com>
2025-06-12 02:48:24 +0200

    2015 - day 2

--- aoc2015/src/day02.hs
+++ aoc2015/src/day02.hs
@@ -1,16 +1,47 @@
 module Day02 where
+import qualified Data.Text as T
+import           Data.Text (Text)
+import Data.Function ((&))
+import Data.List (sort)
 
-parse s =
-  error "Parser not implemented"
+newtype Measurement = Measurement [Int]
 
-part1 i =
-  error "Part 1 not implemented"
+fromList' :: [Text] -> Measurement
+fromList' = Measurement . map (read . T.unpack)
 
-part2 i =
-  error "Part 2 not implemented"
+toList' :: Measurement -> [Int]
+toList' (Measurement [l, w, h]) = [l, w, h]
+
+areas :: Measurement -> [Int]
+areas (Measurement [l, w, h]) = [l * w, w * h, h * l]
+areas _ = error "found more than 3 measurements"
+
+volume :: Measurement -> Int
+volume = product . toList'
+
+parse s = s
+  & T.lines
+  & map (fromList' . T.split (=='x'))
+
+-- Surface area + minum area
+part1' :: Measurement -> Int
+part1' ms =
+  sum (map (*2) as) + minimum as
+  where as = areas ms
+
+part1 :: [Measurement] -> Int
+part1 = sum . map part1'
+
+part2' :: Measurement -> Int
+part2' ms =
+  sum (map (*2) smallest) + volume ms
+  where smallest = (take 2 . sort . toList') ms
+
+part2 :: [Measurement] -> Int
+part2 = sum . map part2'
 
 main :: IO ()
 main = do
   input <- readFile "./inputs/day02.txt"
-  print $ part1 $ parse input
-  -- print $ part2 $ parse input
+  -- print $ part1 $ parse $ T.pack input
+  print $ part2 $ parse $ T.pack input
